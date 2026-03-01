60e674ba4fc9af5f20aa1ad853872cce5c5ae691
Manfred Steyer <manfred.steyer@gmx.net>
2026-03-01 12:34:58 +0100

    refactor: clean up constants

--- src/app/engine/constants.ts
+++ src/app/engine/constants.ts
@@ -1,13 +1,7 @@
-export const VELOCITY_Y = 2;
 export const SCALE = 3;
 export const HERO_PADDING = 2;
 export const COIN_PADDING = 4;
 export const GUMBA_SPEED = 0.4;
-/** Sprung: Anfangsgeschwindigkeit nach oben. */
-export const JUMP_VELOCITY = 2;
-/** Sprung: alle 100 ms wird die Aufwärtsgeschwindigkeit um diesen Wert verringert (Gravity-Effekt). */
-export const JUMP_GRAVITY_PER_100MS = 0.01;
-export const JUMP_DURATION_MS = 500;
 
 export const FALL_GRAVITY = 0.1;
 
